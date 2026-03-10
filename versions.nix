7be12e339c47b49272bb7152ae87a2807dcebbb3
x13-me <17013579+x13-me@users.noreply.github.com>
2026-03-10 11:55:48 +0000

    chore(update): helium to null

--- versions.nix
+++ versions.nix
@@ -1,13 +1,13 @@
 {
-  version = "0.9.4.1";
+  version = "null";
   systems = {
     aarch64-linux = {
-      appimage = "sha256-BvU0bHtJMd6e09HY+9Vhycr3J0O2hunRJCHXpzKF8lk=";
-      tarball  = "sha256-FZMiIwbXmbMHAmC9YPJC5uRHy1W9ZIB0efwGFrCaYKc=";
+      appimage = "";
+      tarball  = "";
     };
     x86_64-linux = {
-      appimage = "sha256-N5gdWuxOrIudJx/4nYo4/SKSxakpTFvL4zzByv6Cnug=";
-      tarball  = "sha256-qXuDUtank46O87jASxczmVMk0iD4JaZi2j9LSBe9VCM=";
+      appimage = "";
+      tarball  = "";
     };
   };
 }
