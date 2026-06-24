f1981e2c533c1845a60aaa2aa3ebad4a977068a7
x13-me <17013579+x13-me@users.noreply.github.com>
2026-06-24 18:18:44 +0000

    chore(update): helium to 0.13.5.1

--- versions.nix
+++ versions.nix
@@ -1,13 +1,13 @@
 {
-  version = "0.13.4.1";
+  version = "0.13.5.1";
   systems = {
     aarch64-linux = {
-      appimage = "sha256-e6rBc9q81Y0X/R8cxrmBYGmzW2TL8sw49rQ0dlKdZLA=";
-      tarball  = "sha256-xmHD65JUIG4vuV/IbKIDvoWIqMe15C5qB5+jcHXDvkk=";
+      appimage = "sha256-xEmYjpUjrbVSz39QlL7/fJORvKWnXkNXUXkPAlEvwEg=";
+      tarball  = "sha256-lWoIEosllPye+8unnxiOse2ek73nD2x96XAngGuDVE4=";
     };
     x86_64-linux = {
-      appimage = "sha256-z23up+T6bj6F+cQslmI92bEksIAw1OQHRIrmQSaaxY8=";
-      tarball  = "sha256-rt//wcAnH7n1ol/PfP37axHpIUKrWXSQN6SisGtE7hw=";
+      appimage = "sha256-VAncL78nyXRRNUPZ2c0QudxFrfxy7tXE2NFN4teeezk=";
+      tarball  = "sha256-70kkeycnB7Ouzt047NpD8O/5t5MO9sE8Qxcby7v3/mY=";
     };
   };
 }
