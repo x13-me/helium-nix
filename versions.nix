a58b47900fca54ad52a21d64330644493d25e169
x13-me <17013579+x13-me@users.noreply.github.com>
2026-08-24 17:06:31 +0000

    chore(update): helium to 0.15.7-1
    unpacking 'github:nixos/nixpkgs/56c02bc00adcf003215cc4bd996d6efaf4cff188' into the Git cache...
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/ffb3c9b' (2026-08-19)
      → 'github:nixos/nixpkgs/56c02bc' (2026-08-23)

--- versions.nix
+++ versions.nix
@@ -1,13 +1,13 @@
 {
-  version = "0.15.6.1";
+  version = "0.15.7.1";
   systems = {
     aarch64-linux = {
-      appimage = "sha256-J10Xxl1Ks7JPp4tC3/ObayNsqgxkU7FpWRhnE439LUc=";
-      tarball  = "sha256-Y1nU48e8UsUtQ67zRHVh5ul0vRMD4jfAe87L2p9oIf4=";
+      appimage = "sha256-lbTmUyf/YCZdEVOfGoRNxoc8T3pIwD4Xo/1w/XOcZE8=";
+      tarball  = "sha256-CYHUDYEMwe+2//4cDPMS2VqcRfbRJD0USDxDJ7kKu6Q=";
     };
     x86_64-linux = {
-      appimage = "sha256-OqXMEZOoFu6NZAozde3ApjNWcvivIItIyeG0HbADpDU=";
-      tarball  = "sha256-TVzi5ZWyvFsh6ovoBhU5DW0k3wlQ5/gUDD4+ykAQPTY=";
+      appimage = "sha256-+zGEGfhIiZWE8mUYb3HrkoM7reFBMdfXlgw3KWT0T98=";
+      tarball  = "sha256-MQKJLN69/g1R46Y44ADo1UvKAXxS2vKsq/XlAwBO+58=";
     };
   };
 }
